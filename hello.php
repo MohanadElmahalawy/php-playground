f0dc714899a67a7a634e8a46c81cda88892f8ba2
MohanadElmahalawy <mohanadtariq12@gmail.com>
2025-07-17 21:40:27 +0300

    echo vs print

---
+++ hello.php
@@ -0,0 +1,7 @@
+<?php
+//1(echo vs print)
+// In print we can only print one string at a time, so we cannot use commas like in echo.
+echo "Hello World!" . "\n" . "ay haga" . "\n" . "ayhaga2";
+print "Hello World!" . "\n" . "ay haga" . "\n" . "ayhaga2";
+echo "Hello, World!", "ayhaag","\n";
+?>
