1874e40cd6144472e663180be58bb95ea02df121
MohanadElmahalawy <mohanadtariq12@gmail.com>
2025-07-17 21:41:17 +0300

    Variables

--- hello.php
+++ hello.php
@@ -4,4 +4,16 @@
 echo "Hello World!" . "\n" . "ay haga" . "\n" . "ayhaga2";
 print "Hello World!" . "\n" . "ay haga" . "\n" . "ayhaga2";
 echo "Hello, World!", "ayhaag","\n";
+
+// 2(primitive variable)
+$ayhaga = "Hello, World!";
+$ayhaga=42;
+$ayhaga = 3.14;
+$ayhaga = null;
+$ayhaga = true;
+echo $ayhaga . "\n";
+
+// (Non-primitive)
+$ayhaga_arr = array("honda", "toyota", "mazda");
+echo "array: " . $ayhaga_arr[1] . "\n";
 ?>
